e916af0bcdc80579535e16e1d63f74bc24d0ac74
DESKTOP-OS81SJJ\TK <thiwanka.kaushal.mob@gmail.com>
2024-12-24 20:55:21 +0530

    Update

--- app/src/main/java/lk/thiwak/megarunii/network/API.kt
+++
@@ -1,4 +0,0 @@
-package lk.thiwak.megarunii.network
-
-class API {
-}
--- src/main.kt
+++ src/main.kt
@@ -1,2 +1,9 @@
-class Main {
+
+fun main() {
+    val name = "Kotlin"
+    println("Hello, $name!")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
 }
